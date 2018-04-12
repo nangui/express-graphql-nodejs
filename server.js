a897c54714b25f271dc80635fb278f69c7c615b3
Adonai Nangui <adonainangui03@gmail.com>
2018-04-12 15:19:38 +0000

    Ajout et configuration de Apollo Server Express

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 import express from 'express'
+import bodyParser from 'body-parser'
+import { graphqlExpress, graphiqlExpress } from 'apollo-server-express'
 
 const app = express()
 
-console.log(`Server changing`)
+app.use('/graphiql', graphiqlExpress({
+    endpointURL: 'graphql'
+}))
+
+app.use('/graphql', bodyParser.json(), graphqlExpress({}))
 
 app.listen(3000, () => {
     console.log(`listning on port 3000`)
